2903cd7a49c193626f23c4879f602004f8f00f01
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2021-11-28 18:36:27 +0800

    I'm done at putting my intro and asking for the question, gonna continue this after I cook

---
+++ prog1_countingVowels.py
@@ -0,0 +1,13 @@
+#Create a program that ask for a sentence as input.
+#Display the number of words, vowels and consonants in the input 
+#ex.
+#input: Bahala kayo dyan
+#output:
+#words: 3
+#vowels: 6
+#consonants: 8
+
+#STEPS:
+#1. Ask for the sentence:
+into = print("Hello, I am Krizzy and I will help you to count the vowels, consonants and words in your sentence")
+sntnce = input("Please put your sentence here: ")
