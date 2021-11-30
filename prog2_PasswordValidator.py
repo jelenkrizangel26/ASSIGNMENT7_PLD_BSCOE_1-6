8135cbf68ab41fbfa1eee723ac965856024c3a1a
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2021-11-30 19:58:46 +0800

    Finished: Done with designing and checking the program :>

--- prog2_PasswordValidator.py
+++ prog2_PasswordValidator.py
@@ -10,8 +10,8 @@
 
 #STEPS:
 #1. Ask for the password:
-print("Good day, kindly input your password below\U0001F601")
-password = input("\n\33[3mYour password\33[0m: ")
+print("\33[94mGood day, kindly input your password below\U0001F607.")
+password = input("\n\33[3m\33[35mYour password\33[0m:\33[36m ")
 
 
 #2. Give the conditions:
@@ -37,11 +37,11 @@ def passwordValid(password):
 
 def main():
     if (passwordValid(password)):
-        print("Welcome! Your password is valid.")
+        print("\n\33[1m\33[34mWelcome! Your password is valid.\33[0m\U0001F601")
 
     else:
-        print("Sorry, it seems like you entered an invalid password.")
-        print("\nYour password must be gretater than 15 letters and contains at least one number, captail letter, and special characters")
+        print("\n\33[1m\33[31mSorry, it seems like you entered an invalid password.\33[0m\U0001F600")
+        print("It must be grater than \33[3m\33[1m\33[92m15 letters\33[0m and contains at least \33[3m\33[1m\33[92mone number\33[0m, \33[3m\33[1m\33[92mcapital letter\33[0m, and \33[3m\33[1m\33[92mspecial characters\33[0m.")
 
 main()
 
